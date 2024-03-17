46ea9a8360359c5d9fe3210ebf0e9d325f387583
hiteshkumar8642 <hiteshkumar8642@gmail.com>
2024-03-17 20:27:00 +0530

    Update urls.py

--- HospitalSystem/urls.py
+++ HospitalSystem/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('dashboard.urls')),
     path('admin/', admin.site.urls),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns=urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
