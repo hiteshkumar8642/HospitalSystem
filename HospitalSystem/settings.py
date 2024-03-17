d0c5fb11083c3430b3396b6463a87c4cea2d6362
hiteshkumar8642 <hiteshkumar8642@gmail.com>
2024-03-17 20:09:34 +0530

    updated requirements.txt

--- HospitalSystem/settings.py
+++ HospitalSystem/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
 from pathlib import Path
+from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,13 +77,15 @@ WSGI_APPLICATION = 'HospitalSystem.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hospital-system',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST':'localhost'
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': config('NAME'),
+    'USER': config('USER'),
+    'PASSWORD': config('PASSWORD'),
+    'HOST': config('HOST'),
+    'PORT': '5432',
+    'OPTIONS': {'sslmode': 'require'},
+  }
 }
 
 
